b4413214903a84806d414e16f3353b7a7f86d804
Jayant Dewangan <126174563+JayantDewangan@users.noreply.github.com>
2025-07-05 16:12:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+// name validation using regex : 
+function isValidName(name){
+    let regex = /^[a-zA-z\s]{2,}$/;
+    return regex.test(name.trim())
+}
+
+// email validation using regex : 
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+}
+
+// phone number validation : 
+function isValidPhone(phone){
+    let regex = /^\d{10}$/;
+    return regex.test(phone.trim());
+}
+
+// password validation : 
+function isValidPassword(password){
+    let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return regex.test(password);
+}
+
+// repeated styling function : 
+function showError(message) {
+  $("#errorMsg").css({
+    width: "250px",
+    height: "auto",
+    backgroundColor: "#de4043",
+    borderRadius: "10px",
+    border: "transparent",
+    boxShadow: "2px 2px 4px #414141",
+    padding: "10px",
+    fontSize: "18px",
+    color: "#F5F5DC",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    gap: "5px"
+  }).fadeIn(200);
+
+  $("#para1").html("✖️");
+  $("#para2").html(message);
+}
+
+
+// button click : 
+const submit = $("#submit")
+submit.click((e)=>{
+    e.preventDefault();// Prevent form from submitting
+
+    const name = isValidName($("#name").val());
+    if (!name) return showError("Please enter a valid name!!");
+
+    const email = isEmail($("#email").val());
+    if (!email) return showError("Please enter a valid email id!!");
+
+    const phone = isValidPhone($("#phone").val());
+    if (!phone) return showError("Phone number must be exactly 10 digits.");
+
+    const password = isValidPassword($("#password").val());
+    if (!password) {
+        return showError("Password must be at least 8 characters and include uppercase, lowercase, a number, and a special character.");
+    }
+
+
+    // All validations passed
+    $("#errorMsg").css({
+        width: "250px",
+        height: "auto",
+        backgroundColor: "#3f9632",
+        borderRadius: "10px",
+        border: "transparent",
+        boxShadow: "2px 2px 4px #414141",
+        padding: "10px",
+        fontSize: "18px",
+        color: "#F5F5DC",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "5px",
+        textAlign: "center"
+    }).fadeIn(200);
+
+    $("#para1").html("✔️💐");
+    $("#para2").html("FORM SUBMITTED SUCCESSFULLY!!");
+});
+
+// show password functionality : 
+$("#togglePassword").change(function () {
+    if ($(this).is(":checked")) {
+        $("#password").attr("type", "text"); // Show password
+    } else {
+        $("#password").attr("type", "password"); // Hide password
+    }
+});
